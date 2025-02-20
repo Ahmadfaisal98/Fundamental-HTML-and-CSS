7ea275451c80639ce2fb1ebecde6c99daedb6c56
Ahmad Faisal <achmad_fsl@yahoo.com>
2025-02-20 22:34:00 +0700

    add index2.html

--- calculators/calculator2/script.js
+++ calculators/calculator2/script.js
@@ -1,32 +1,25 @@
 const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('button')
 let currentInput = ''
 let operator = ''
 let firstValue = ''
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const value = button.textContent
+function addNumber(value) {
+  currentInput += value
+  display.value = currentInput
+}
 
-    if (value === 'C') {
-      currentInput = ''
-      firstValue = ''
-      operator = ''
-      display.value = ''
-    } else if (value === '=') {
-      if (operator && firstValue !== '') {
-        currentInput = eval(`${firstValue} ${operator} ${currentInput}`)
-        display.value = currentInput
-        firstValue = ''
-        operator = ''
-      }
-    } else if (['+', '-', '*', '/'].includes(value)) {
-      operator = value
-      firstValue = currentInput
-      currentInput = ''
-    } else {
-      currentInput += value
-      display.value = currentInput
-    }
-  })
-})
+function clearDisplay() {
+  currentInput = ''
+  firstValue = ''
+  operator = ''
+  display.value = ''
+}
+
+function calculate() {
+  if (operator && firstValue !== '') {
+    currentInput = eval(`${firstValue} ${operator} ${currentInput}`)
+    display.value = currentInput
+    firstValue = ''
+    operator = ''
+  }
+}
